dfb03f4d600366e8302bb607e860c846756ae294
Jon Berardinelli <jon.berardinelli014@yahoo.com>
2018-12-04 20:21:50 -0500

    Work done in IT1050

---
+++ 12-4-18/12-4-18/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _12_4_18
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Person[] people;
+            people = new Person[3];
+            people[0] = new Person("Nick", "Smith");
+            people[1] = new Person("Joe", "Smith");
+            people[2] = new Person("Dianne", "Smith");
+
+            foreach (Person names in people)
+            {
+                names.PrintFullName();
+            }
+
+            System.Console.ReadKey();
+
+        }
+    }
+}
