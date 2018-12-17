ae2870a65df19e0ba50d9e20c6a2746255808655
Jonathon Berardinelli <jon.berardinelli014@gmail.com>
2018-12-16 20:14:04 -0500

    Completed Final Project

---
+++ IT_1050_Final_Project/IT_1050_Final_Project/Elevator.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IT_1050_Final_Project
+{
+    public class Elevator
+    {
+        private double MaxWeight { get; set; }
+        private Passenger[] Occupants;
+
+        public double sum { get; set; }
+
+        public Elevator( int maxOccupants, double maxWeight)
+        {
+            this.Occupants = new Passenger[maxOccupants];
+            this.MaxWeight = maxWeight;
+        }
+
+        public void AddOccupant( Passenger passenger, int index)
+        {
+            this.Occupants[index] = passenger;
+        }
+
+        public double GetCurrentWeight()
+        {
+            for (int i = 0; i < Occupants.Length; i++)
+            {
+                sum += Occupants[i].GetWeight();
+            }    
+            return sum;
+        }
+        public bool IsOverMaxCapacity()
+        {
+            bool OverCapacity = false;
+
+            if (GetCurrentWeight() > MaxWeight)
+            {
+                OverCapacity = true;
+            }
+
+            return OverCapacity;
+        }
+
+    }
+}
---
+++ IT_1050_Final_Project/IT_1050_Final_Project/Passenger.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IT_1050_Final_Project
+{
+    public class Passenger
+    {
+        private string Name { get; set; }
+        private double Weight { get; set; }
+
+        public Passenger (string name, double weight)
+        {
+            this.Name = name;
+            this.Weight = weight;
+        }
+
+        public string GetName()
+        {
+            return Name;
+        }
+
+        public double GetWeight()
+        {
+            return Weight;
+        }
+    }
+}
